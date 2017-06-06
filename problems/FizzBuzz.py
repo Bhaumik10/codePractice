212aa7c7817c1919dc7f3bdcefceff107955be8d
bhaumik-7p <bp1232@nyu.edu>
2017-06-06 14:31:18 -0400

    add comments & structure code

--- problems/FizzBuzz.py
+++ problems/FizzBuzz.py
@@ -1,22 +1,22 @@
-Write a program that outputs the string representation of numbers from 1 to n.
+## Problem Statement
+## Write a program that outputs the string representation of numbers from 1 to n.
+## But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
 
-But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
-
-Solution::
+## Solution::
 
 def fizzBuzz(self,n):
-    for i in xrange(1,n+1):
-        if i % 15 == 0:
-            n = "FizzBuzz"
+    for number in xrange(1,n+1): ## We will start the Index with 1.
+        if number % 15 == 0:
+            n_ = "FizzBuzz"
             self.append(n)
-        elif i % 3 == 0:
-            n = "Fizz"
+        elif number % 3 == 0:
+            n_ = "Fizz"
             self.append(n)
-        elif i % 5 == 0:
-            n = "Buzz"
+        elif number % 5 == 0:
+            n_ = "Buzz"
             self.append(n)
         else:
-            self.append(str(i))
+            self.append(str(number))
     return self
 
 answer = fizzBuzz([],15)
